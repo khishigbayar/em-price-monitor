2cbb94c880a03514f82e3b7fdfcc00c7b8e7c6ea
khishigbayar <khishigbayr@gmail.com>
2026-01-07 11:18:02 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -107,19 +107,19 @@ def get_products():
     return [
         {
             "product_url": "https://em.hdc.gov.mn/productMap/113",
-            "product_name": "Виферон 500,000"
+            "product_name": "Аминовит"
         },
         {
             "product_url": "https://em.hdc.gov.mn/productMap/1155",
-            "product_name": "Виферон 1,000,000"
+            "product_name": "Урокер"
         },
         {
             "product_url": "https://em.hdc.gov.mn/productMap/2017",
-            "product_name": "Виферон 3,000,000"
+            "product_name": "Панпирин Кю"
         },
         {
             "product_url": "https://em.hdc.gov.mn/productMap/2344",
-            "product_name": "Виферон 150,000"
+            "product_name": "Альбуман"
         }
     ]
 
